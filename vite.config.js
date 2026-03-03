a42b14548a5b72a07bd9b1bc62800e6124793c8c
Sarvar <sarvarakmalovich523@gmail.com>
2026-03-03 13:18:57 +0500

    chore: optimize build chunks and increase size limit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('recharts')) return 'vendor_charts';
+            if (id.includes('framer-motion')) return 'vendor_animation';
+            if (id.includes('react')) return 'vendor_react';
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
